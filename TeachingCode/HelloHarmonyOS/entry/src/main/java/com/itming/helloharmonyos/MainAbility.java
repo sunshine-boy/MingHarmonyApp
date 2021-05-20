54ca322308e1093aaa0a9570f20394d322c2141c
BXMing <649579772@qq.com>
2021-05-20 09:21:46 +0800

    build HelloHarmonyOS source code

---
+++ TeachingCode/HelloHarmonyOS/entry/src/main/java/com/itming/helloharmonyos/MainAbility.java
@@ -0,0 +1,13 @@
+package com.itming.helloharmonyos;
+
+import com.itming.helloharmonyos.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
---
+++ TeachingCode/HelloHarmonyOS/entry/src/test/java/com/itming/helloharmonyos/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.itming.helloharmonyos;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
